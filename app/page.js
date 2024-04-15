d6b60bbafe89d2ad6f2900dabb2c55a341c15856
Akshay Thakur <akshaythakur@192.168.1.11>
2024-04-15 20:11:08 +0530

    updated

--- app/page.js
+++ app/page.js
@@ -1,12 +1,7 @@
-import Sidenav from "./(pages)/_components/Sidenav";
 export default function Home({ children }) {
   return (
     <div className="bg-black">
-      <div className=" h-full md:w-64 flex-col fixed inset-y-0 z-50">
-        <Sidenav />
-      </div>
-
-      <div className="ml-64">{children}</div>
+      <div>{children}</div>
     </div>
   );
 }
