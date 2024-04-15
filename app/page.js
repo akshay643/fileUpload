66ca83bc13648c88a9c3e1f9832d4885293fc00a
Akshay Thakur <akshaythakur@192.168.1.11>
2024-04-15 20:19:33 +0530

    updated

--- app/page.js
+++ app/page.js
@@ -1,7 +1,16 @@
-export default function Home({ children }) {
+// home/page.js
+
+// Import necessary dependencies if needed
+import React from "react";
+
+// Define your page component
+const Home = ({ children }) => {
   return (
     <div className="bg-black">
       <div>{children}</div>
     </div>
   );
-}
+};
+
+// Export the Home component as the default export
+export default Home;
