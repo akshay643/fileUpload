643728bc91aa95116a7415dd8931c7c2faec03eb
Akshay Thakur <akshaythakur@192.168.1.11>
2024-04-15 20:16:19 +0530

    updated

--- app/layout.js
+++ app/layout.js
@@ -15,11 +15,7 @@ export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>
-          {/* <Header /> */}
-          {children}
-          {/* <Footer /> */}
-        </body>
+        <body className={inter.className}>{children}</body>
       </html>
     </ClerkProvider>
   );
